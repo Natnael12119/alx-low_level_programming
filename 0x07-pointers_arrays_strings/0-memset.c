f83603a9a3b8a17c84da0b7a928ada746db0a0d7
Natnael12119 <nhattydeveloper@gmail.com>
2023-01-03 06:41:40 -0800

    Trying to fix a bug

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,13 +5,15 @@
 
 #include "main.h"
 
-/**
+/*
  * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
- * Return: Always 0 (Success)
+ * @s: pointer to memory area to fill with b
+ * @b: value to fill memory area with
+ * @n: number of bytes to fill with b
+ *
+ * Return: pointer to memory area s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
