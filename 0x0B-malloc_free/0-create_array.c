9e46d64250dc5ade6e6d6db10f06e6c6ee31660f
Natnael12119 <nhattydeveloper@gmail.com>
2023-04-11 10:41:22 -0700

    Bug fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,12 +7,15 @@
  * @b: unsigned int type
  * Return: return pointer
  */
-void *create_array(unsigned int b)
+void *create_array(unsigned int size, char c)
 {
-	int *ptr;
+	char *str;
+	unsigned int i;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
