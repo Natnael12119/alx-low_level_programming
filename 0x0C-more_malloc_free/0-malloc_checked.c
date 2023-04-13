f1c6ca2fadf453c8d8ecdbcd31aa4471b020428a
Natnael12119 <nhattydeveloper@gmail.com>
2023-04-13 11:06:03 -0700

    Bug on 0-malloc_checked.c is fixed

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,35 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Main Entry
- * @b: input
- * Return: 0
- */
-void *malloc_checked(unsigned int b)
-{
-	void *p;
 
-	p = malloc(b);
-	if (p == NULL)
+ * *malloc_checked - allocates memory using malloc
+
+ * @b: number of bytes to allocate
+
+ *
+
+ * Return: a pointer to the allocated memory
+
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	void *ptr;
+
+
+
+	ptr = malloc(b);
+
+
+
+	if (ptr == NULL)
+
 		exit(98);
-	return (p);
-}}
+
+
+
+	return (ptr);
+
+}
