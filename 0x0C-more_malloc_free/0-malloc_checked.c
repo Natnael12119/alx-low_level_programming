a25d2cb364763438e003744946f4f36e500213cd
Natnael12119 <nhattydeveloper@gmail.com>
2023-04-13 11:01:33 -0700

    Bug fixed

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,40 +2,16 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Main Entry
- * @s1: input
- * @s2: input
- * @n: input
+ * malloc_checked - Main Entry
+ * @b: input
  * Return: 0
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-	unsigned int i, j, k;
-	char *s;
+	void *p;
 
-	if (s1 == NULL)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
-	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-			;
-	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + i] = s2[k];
-	s[i + j] = '\0';
-	return (s);
-}
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}}
