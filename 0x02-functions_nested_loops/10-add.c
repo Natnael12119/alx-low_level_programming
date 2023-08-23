d946db782ca0b21e7841f07e6c2e20ad223f30e5
Natnael12119 <nhattydeveloper@gmail.com>
2023-08-23 03:52:45 -0700

    All mandatory and advanced tasks of 0x02. C - Functions, nested loops has done.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - A function that sums up two numbers
+*@n: A first integer
+*@m: A second integer
+*Return: returns 0
+*/
+int add(int n, int m)
+{
+return (n + m);
+}
